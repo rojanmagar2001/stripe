0d7a63602b5884cac56d60e8355fda328c1c0b71
neha gupta <neha.system25@gmail.com>
2023-04-24 13:16:43 +0545

    first changes

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+// Product Details 
+// Minimum amount is $0.50 US 
+$itemName = "Demo Product";
+$itemNumber = "PN12345";
+$itemPrice = 25;
+$currency = "USD";
+// Stripe API configuration  
+define('STRIPE_API_KEY', 'sk_test_51MknPOFI38iOSVZE1XxrzuFd17FZw55rp40E55og8p5AK43WeSoXPXUhPgcYSTmRSaaJoJnXyKMAY5QGCXmAw47R00Y8cp0bec');
+define('STRIPE_PUBLISHABLE_KEY', 'pk_test_51MknPOFI38iOSVZE71CYisqkIoufTo5bknwmqDFBMN1HxNsWtbZwfKyuSXlniNYsW2Bn0ueK4VJMjvlqhgaEiBBa00MpkgGUMb');
+
+// Database configuration  
+define('DB_HOST', 'localhost');
+define('DB_USERNAME', 'roja7944');
+define('DB_PASSWORD', 'xm()n$@&(bcK');
+define('DB_NAME', 'roja7944_stripe');
